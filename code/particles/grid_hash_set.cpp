c080cd5ab5bc87b1f02a9bc942758ceceaa27501
Joel Pettersson <petterssonjoel@gmail.com>
2013-01-23 20:00:27 +1000

    Reformat GridHashSet.

--- code/particles/grid_hash_set.cpp
+++ code/particles/grid_hash_set.cpp
@@ -7,226 +7,229 @@
 
 namespace prtcl {
 
-    /**
-     * Constructs a new grid hash set with the given number of particles and
-     * the specified size.
-     */
-    GridHashSet::GridHashSet(int n, double size, double cutoff_radius) :
-        num_rows(ceil(sqrt(n))), num_cols(ceil(sqrt(n))),
-        grid_size(size/num_rows),
-        num_surrounding_grids(ceil(cutoff_radius/grid_size)) { 
+/**
+ * Constructs a new grid hash set with the given number of particles and
+ * the specified size.
+ */
+GridHashSet::GridHashSet(int n, double size, double cutoff_radius) :
+		num_rows(ceil(sqrt(n))), num_cols(ceil(sqrt(n))), grid_size(
+				size / num_rows), num_surrounding_grids(
+				ceil(cutoff_radius / grid_size)) {
 
-        grids = new std::vector<std::deque<particle_t *>*>(num_rows * num_cols);
+	grids = new std::vector<std::deque<particle_t *>*>(num_rows * num_cols);
 
-        std::vector<std::deque<particle_t *>*>::iterator it;
-        for (it = grids->begin(); it != grids->end(); ++it) {
-            *it = new std::deque<particle_t *>();
-        }
-    }
+	std::vector<std::deque<particle_t *>*>::iterator it;
+	for (it = grids->begin(); it != grids->end(); ++it) {
+		*it = new std::deque<particle_t *>();
+	}
+}
 
-    /**
-     * Deconstructs this grid hash set.
-     */
-    GridHashSet::~GridHashSet() {
+/**
+ * Deconstructs this grid hash set.
+ */
+GridHashSet::~GridHashSet() {
 
-        std::vector<std::deque<particle_t *>*>::iterator it;
-        for (it = grids->begin(); it != grids->end(); ++it) {
-            delete *it;
-        }
+	std::vector<std::deque<particle_t *>*>::iterator it;
+	for (it = grids->begin(); it != grids->end(); ++it) {
+		delete *it;
+	}
 
-        delete grids;
-    }
+	delete grids;
+}
 
-    /**
-     * Removes all particle pointers from this grid hash set.
-     */
-    void GridHashSet::clear() {
-        std::vector<std::deque<particle_t *>*>::iterator it;
-        for (it = grids->begin(); it != grids->end(); ++it) {
-            (*it)->clear();
-        }
-    }
+/**
+ * Removes all particle pointers from this grid hash set.
+ */
+void GridHashSet::clear() {
+	std::vector<std::deque<particle_t *>*>::iterator it;
+	for (it = grids->begin(); it != grids->end(); ++it) {
+		(*it)->clear();
+	}
+}
 
-    /**
-     * Inserts pointer to the given particle p into this grid hash set.
-     */
-    void GridHashSet::insert(particle_t & p) {
-        (*grids)[get_index(p)]->push_back(&p);
-    }
+/**
+ * Inserts pointer to the given particle p into this grid hash set.
+ */
+void GridHashSet::insert(particle_t & p) {
+	(*grids)[get_index(p)]->push_back(&p);
+}
 
-    /**
-     * Returns an iterator to the beginning of the sequence of particles that
-     * the given particle belongs to, including itself.
-     */
-    std::deque<particle_t *>::iterator GridHashSet::grid_particles_begin(particle_t & p) {
-        return (*grids)[get_index(p)]->begin();
-    }
+/**
+ * Returns an iterator to the beginning of the sequence of particles that
+ * the given particle belongs to, including itself.
+ */
+std::deque<particle_t *>::iterator GridHashSet::grid_particles_begin(
+		particle_t & p) {
+	return (*grids)[get_index(p)]->begin();
+}
 
-    std::vector<std::deque<particle_t *>*>::iterator GridHashSet::grids_begin() {
-        return grids->begin();
-    }
+std::vector<std::deque<particle_t *>*>::iterator GridHashSet::grids_begin() {
+	return grids->begin();
+}
 
-    std::vector<std::deque<particle_t *>*>::iterator GridHashSet::grids_end() {
-        return grids->end();
-    }
+std::vector<std::deque<particle_t *>*>::iterator GridHashSet::grids_end() {
+	return grids->end();
+}
 
-    GridHashSet::surr_iterator GridHashSet::surr_begin(particle_t & p) {
-        return surr_iterator(this, p);
-    }
+GridHashSet::surr_iterator GridHashSet::surr_begin(particle_t & p) {
+	return surr_iterator(this, p);
+}
 
-    GridHashSet::surr_iterator GridHashSet::surr_end(particle_t & p) {
-        return surr_iterator(this, p).ff_to_end();
-    }
+GridHashSet::surr_iterator GridHashSet::surr_end(particle_t & p) {
+	return surr_iterator(this, p).ff_to_end();
+}
 
-    /**
-     * Stores the top left surrounding row and column of p in tlr and tlc.
-     */
-    void GridHashSet::first_surr_index(int& tlr, int& tlc, particle_t& p) {
-        tlr = max(get_row(p) - num_surrounding_grids, 0);
-        tlc = max(get_col(p) - num_surrounding_grids, 0);
-    }
+/**
+ * Stores the top left surrounding row and column of p in tlr and tlc.
+ */
+void GridHashSet::first_surr_index(int& tlr, int& tlc, particle_t& p) {
+	tlr = max(get_row(p) - num_surrounding_grids, 0);
+	tlc = max(get_col(p) - num_surrounding_grids, 0);
+}
 
-    void GridHashSet::last_surr_index(int& brr, int& brc, particle_t& p) {
-        brr = min(get_row(p) + num_surrounding_grids, num_rows - 1);
-        brc = min(get_col(p) + num_surrounding_grids, num_cols - 1);
-    }
+void GridHashSet::last_surr_index(int& brr, int& brc, particle_t& p) {
+	brr = min(get_row(p) + num_surrounding_grids, num_rows - 1);
+	brc = min(get_col(p) + num_surrounding_grids, num_cols - 1);
+}
 
-    int GridHashSet::get_index(int row, int col) {
-        return row * num_cols + col;
-    }
+int GridHashSet::get_index(int row, int col) {
+	return row * num_cols + col;
+}
 
-    /**
-     * Returns the internal vector index where p should reside.
-     */
-    int GridHashSet::get_index(particle_t & p) {
-        return get_index(get_row(p), get_col(p));
-    }
+/**
+ * Returns the internal vector index where p should reside.
+ */
+int GridHashSet::get_index(particle_t & p) {
+	return get_index(get_row(p), get_col(p));
+}
 
-    /**
-     * Returns the row index where p should reside.
-     */
-    int GridHashSet::get_row(particle_t & p) {
-        assert(floor(p.y / grid_size) < num_rows);
-        return floor(p.y / grid_size);
-    }
+/**
+ * Returns the row index where p should reside.
+ */
+int GridHashSet::get_row(particle_t & p) {
+	assert(floor(p.y / grid_size) < num_rows);
+	return floor(p.y / grid_size);
+}
 
-    /**
-     * Returns the column index where p should reside.
-     */
-    int GridHashSet::get_col(particle_t & p) {
-        assert(floor(p.x / grid_size) < num_cols);
-        return floor(p.x / grid_size);
-    }
+/**
+ * Returns the column index where p should reside.
+ */
+int GridHashSet::get_col(particle_t & p) {
+	assert(floor(p.x / grid_size) < num_cols);
+	return floor(p.x / grid_size);
+}
 
+/*
+ * Surrounding iterator.
+ */
 
-    /*
-     * Surrounding iterator.
-     */
+/**
+ * Constructs an empty (useless) surrounding iterator.
+ */
+GridHashSet::surr_iterator::surr_iterator() {
+}
 
-    /**
-     * Constructs an empty (useless) surrounding iterator.
-     */
-    GridHashSet::surr_iterator::surr_iterator() {
-    }
+/**
+ * Constructs a surroudning iterator and initializes row and col bounds.
+ */
+GridHashSet::surr_iterator::surr_iterator(GridHashSet* grid, particle_t& p,
+		bool end) :
+		grid(grid), reached_end(end) {
 
-    /**
-     * Constructs a surroudning iterator and initializes row and col bounds.
-     */
-    GridHashSet::surr_iterator::surr_iterator(GridHashSet* grid, particle_t& p, bool end)
-        : grid(grid), reached_end(end) {
+	// Store the start and end surrounding indices.
+	grid->first_surr_index(top_left_row, top_left_col, p);
+	grid->last_surr_index(bottom_right_row, bottom_right_col, p);
 
-        // Store the start and end surrounding indices. 
-        grid->first_surr_index(top_left_row, top_left_col, p);
-        grid->last_surr_index(bottom_right_row, bottom_right_col, p);
+	// Initialize internal outer "iterator".
+	r = top_left_row;
+	c = top_left_col;
 
-        // Initialize internal outer "iterator".
-        r = top_left_row;
-        c = top_left_col;
+	// Initialize the deque particle iterator.
+	particles_it = grid->grids->at(grid->get_index(r, c))->begin();
+	particles_it_end = grid->grids->at(grid->get_index(r, c))->end();
 
-        // Initialize the deque particle iterator.
-        particles_it = grid->grids->at(grid->get_index(r, c))->begin();
-        particles_it_end = grid->grids->at(grid->get_index(r, c))->end();
+	// While inner deque iterator points nowhere useful.
+	while (particles_it == particles_it_end || *particles_it == NULL) {
+		// Move to next grid.
+		if (++c > bottom_right_col) {
+			c = top_left_col;
+			++r;
+		}
 
+		if (r > bottom_right_row) {
+			reached_end = true;
+		} else {
+			// Update internal deque (particles) iterators.
+			particles_it = grid->grids->at(grid->get_index(r, c))->begin();
+			particles_it_end = grid->grids->at(grid->get_index(r, c))->end();
+		}
+	}
+}
 
-        // While inner deque iterator points nowhere useful.
-        while (particles_it == particles_it_end || *particles_it == NULL) {
-            // Move to next grid.
-            if (++c > bottom_right_col) {
-                c = top_left_col;
-                ++r;
-            }
+GridHashSet::surr_iterator::~surr_iterator() {
+}
 
-            if (r > bottom_right_row) {
-                reached_end = true;
-            } else {
-                // Update internal deque (particles) iterators.
-                particles_it = grid->grids->at(grid->get_index(r, c))->begin();
-                particles_it_end = grid->grids->at(grid->get_index(r, c))->end();
-            }
-        }
-    }
+/**
+ * Prefix ++ operator.
+ *
+ * Guarantees that this iterator will never be in a state where it will
+ * reference a deque iterator that is exhausted.
+ */
+GridHashSet::surr_iterator & GridHashSet::surr_iterator::operator++() {
+	// Increment the inner dequeu (particles) iterator.
+	++particles_it;
 
-    GridHashSet::surr_iterator::~surr_iterator() {
-    }
+	// While inner deque iterator points nowhere useful.
+	while ((particles_it == particles_it_end || *particles_it == NULL)
+			&& !reached_end) {
+		// Move to next grid.
+		if (++c > bottom_right_col) {
+			c = top_left_col;
+			++r;
+		}
 
-    /**
-     * Prefix ++ operator.
-     *
-     * Guarantees that this iterator will never be in a state where it will
-     * reference a deque iterator that is exhausted.
-     */
-    GridHashSet::surr_iterator & GridHashSet::surr_iterator::operator++() {
-        // Increment the inner dequeu (particles) iterator.
-        ++particles_it;
+		if (r > bottom_right_row) {
+			reached_end = true;
+		} else {
+			// Update internal deque (particles) iterators.
+			particles_it = grid->grids->at(grid->get_index(r, c))->begin();
+			particles_it_end = grid->grids->at(grid->get_index(r, c))->end();
+		}
+	}
 
-        // While inner deque iterator points nowhere useful.
-        while ((particles_it == particles_it_end || *particles_it == NULL) && !reached_end) {
-            // Move to next grid.
-            if (++c > bottom_right_col) {
-                c = top_left_col;
-                ++r;
-            }
+	return *this;
+}
 
-            if (r > bottom_right_row) {
-                reached_end = true;
-            } else {
-                // Update internal deque (particles) iterators.
-                particles_it = grid->grids->at(grid->get_index(r, c))->begin();
-                particles_it_end = grid->grids->at(grid->get_index(r, c))->end();
-            }
-        }
+GridHashSet::surr_iterator::pointer GridHashSet::surr_iterator::operator*() const {
+	// Dereference the internal current deque<particle_t*> iterator.
+	return *particles_it;
+}
 
-        return *this;
-    }
+bool GridHashSet::surr_iterator::operator==(surr_iterator const & other) {
+	// Either both have reached end or r, c and particles iterator match.
+	return reached_end == other.reached_end
+			&& (reached_end
+					|| (r == other.r && c == other.c
+							&& particles_it == other.particles_it));
+}
 
-    GridHashSet::surr_iterator::pointer GridHashSet::surr_iterator::operator*() const {
-        // Dereference the internal current deque<particle_t*> iterator.
-        return *particles_it;
-    }
+bool GridHashSet::surr_iterator::operator!=(surr_iterator const & other) {
+	return !((*this) == other);
+}
 
-    bool GridHashSet::surr_iterator::operator==(surr_iterator const & other) {
-        // Either both have reached end or r, c and particles iterator match.
-        return reached_end == other.reached_end && (reached_end || (r == other.r && c == other.c
-            && particles_it == other.particles_it)); 
-    }
+/**
+ * Fast forwards this iterator to the end and returns a reference to itself.
+ */
+GridHashSet::surr_iterator& GridHashSet::surr_iterator::ff_to_end() {
+	// Go one step further than the last grid.
+	r = bottom_right_row + 1;
+	c = top_left_col;
 
-    bool GridHashSet::surr_iterator::operator!=(surr_iterator const & other) {
-        return !((*this) == other);
-    }
+	// Get the deque (particle) iterator from last grid.
+	reached_end = true;
 
-    /**
-     * Fast forwards this iterator to the end and returns a reference to itself.
-     */
-    GridHashSet::surr_iterator& GridHashSet::surr_iterator::ff_to_end() {
-        // Go one step further than the last grid.
-        r = bottom_right_row + 1;
-        c = top_left_col;
-
-        // Get the deque (particle) iterator from last grid.
-        reached_end = true;
-
-        return (*this);
-    }
+	return (*this);
+}
 
 }
 
--- code/particles/grid_hash_set.h
+++ code/particles/grid_hash_set.h
@@ -6,61 +6,61 @@
 #include "common.h"
 
 namespace prtcl {
-    class GridHashSet {
-        public:
-            friend class surr_iterator;
+class GridHashSet {
+public:
+	friend class surr_iterator;
 
-            class surr_iterator : std::iterator_traits<particle_t *> {
-                friend class GridHashSet;
+	class surr_iterator: std::iterator_traits<particle_t *> {
+		friend class GridHashSet;
 
-                public:
-                    surr_iterator();
-                    surr_iterator(GridHashSet *, particle_t &, bool end = false);
-                    //surr_iterator & operator=(const surr_iterator &); // Assignment constructor
-                    //surr_iterator(const surr_iterator &); // Copy-constructor
-                    ~surr_iterator();
-                    surr_iterator & operator++();
-                    pointer operator*() const;
-                    bool operator==(surr_iterator const &);
-                    bool operator!=(surr_iterator const &);
-                private:
-                    GridHashSet * grid;
-                    bool reached_end;
-                    int r, c;
-                    int top_left_row, top_left_col, bottom_right_row, bottom_right_col;
-                    std::deque<particle_t *>::iterator particles_it;
-                    std::deque<particle_t *>::iterator particles_it_end;
+	public:
+		surr_iterator();
+		surr_iterator(GridHashSet *, particle_t &, bool end = false);
+		//surr_iterator & operator=(const surr_iterator &); // Assignment constructor
+		//surr_iterator(const surr_iterator &); // Copy-constructor
+		~surr_iterator();
+		surr_iterator & operator++();
+		pointer operator*() const;
+		bool operator==(surr_iterator const &);
+		bool operator!=(surr_iterator const &);
+	private:
+		GridHashSet * grid;
+		bool reached_end;
+		int r, c;
+		int top_left_row, top_left_col, bottom_right_row, bottom_right_col;
+		std::deque<particle_t *>::iterator particles_it;
+		std::deque<particle_t *>::iterator particles_it_end;
 
-                    surr_iterator& ff_to_end();
-            };
+		surr_iterator& ff_to_end();
+	};
 
-            GridHashSet(int n, double size, double cutoff_radius);
-            ~GridHashSet();
-            //particle_t& operator() (unsigned row, unsigned col);
-            //particle_t const& operator() (unsigned row, unsigned col) const;
-            void clear();
-            void insert(particle_t &);
-            std::deque<particle_t *>::iterator grid_particles_begin(particle_t &);
-            std::vector<std::deque<particle_t *>*>::iterator grids_begin();
-            std::vector<std::deque<particle_t *>*>::iterator grids_end();
-            surr_iterator surr_begin(particle_t &);
-            surr_iterator surr_end(particle_t &);
-            int get_row(particle_t &);
-            int get_col(particle_t &);
+	GridHashSet(int n, double size, double cutoff_radius);
+	~GridHashSet();
+	//particle_t& operator() (unsigned row, unsigned col);
+	//particle_t const& operator() (unsigned row, unsigned col) const;
+	void clear();
+	void insert(particle_t &);
+	std::deque<particle_t *>::iterator grid_particles_begin(particle_t &);
+	std::vector<std::deque<particle_t *>*>::iterator grids_begin();
+	std::vector<std::deque<particle_t *>*>::iterator grids_end();
+	surr_iterator surr_begin(particle_t &);
+	surr_iterator surr_end(particle_t &);
+	int get_row(particle_t &);
+	int get_col(particle_t &);
 
-        private:
-            int num_rows;
-            int num_cols;
-            double grid_size;
-            int num_surrounding_grids;
-            std::vector<std::deque<particle_t *>*>* grids;
+private:
+	int num_rows;
+	int num_cols;
+	double grid_size;
+	int num_surrounding_grids;
+	std::vector<std::deque<particle_t *>*>* grids;
 
-            void first_surr_index(int&, int&, particle_t&);
-            void last_surr_index(int&, int&, particle_t&);
-            int get_index(int row, int col);
-            int get_index(particle_t &);
+	void first_surr_index(int&, int&, particle_t&);
+	void last_surr_index(int&, int&, particle_t&);
+	int get_index(int row, int col);
+	int get_index(particle_t &);
 
-    };
+};
 }
 
 #endif
